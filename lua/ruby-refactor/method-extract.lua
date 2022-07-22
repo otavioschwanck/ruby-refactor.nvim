38df7fcf540cbcd2a3cea2a4bc961edfcd70f790
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-07-22 03:24:24 +0100

    select

--- lua/ruby-refactor/method-extract.lua
+++ lua/ruby-refactor/method-extract.lua
@@ -1,5 +1,12 @@
+local selection_range = require('ruby-refactor.utils').visual_selection_range
+
 local function extract()
-  print('hello')
+  local selected_text = selection_range()
+
+  -- Logic Here
 end
+
+-- Just for texting
+vim.keymap.set('v', 'a', extract, {})
 
 return extract
